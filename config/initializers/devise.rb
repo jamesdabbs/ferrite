72544c2a88150b3e6af636fca8afa9acd599b554
James Dabbs <jamesdabbs@gmail.com>
2015-05-30 19:05:55 -0400

    Disable required OAuth secret key check for CI

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -233,8 +233,10 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :google_oauth2, Figaro.env.google_client_id!, Figaro.env.google_secret!, scope: 'email'
-  config.omniauth :github, Figaro.env.github_client_id!, Figaro.env.github_secret!, scope: 'user:email,read:org'
+  unless ENV["CI"]
+    config.omniauth :google_oauth2, Figaro.env.google_client_id!, Figaro.env.google_secret!, scope: 'email'
+    config.omniauth :github, Figaro.env.github_client_id!, Figaro.env.github_secret!, scope: 'user:email,read:org'
+  end
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
