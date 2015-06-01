5627395960aed11227754dda0e1088e791f261f9
Katie Walsh <katie.katiewalsh@gmail.com>
2015-06-01 15:23:52 -0400

    Assignment homework test

--- test/integration/assigning_homework_test.rb
+++ test/integration/assigning_homework_test.rb
@@ -1,16 +1,28 @@
 require 'test_helper'
 
 class AssigningHomeworkTest < ActionDispatch::IntegrationTest
-  test "creating a new project and assigning it" do
-    instructor = FactoryGirl.create :user, :instructor
-    login instructor
-    visit new_project_path
-    # Fill in form
-    # Save
-    # Create assignment
+  def setup
+    @course = FactoryGirl.create :course
+    @instructor = @course.instructor.user
+    @student = FactoryGirl.create :user
+    FactoryGirl.create :course_member, course: @course, user: @student
+  end
 
-    # Can see assignment
-    # Login as student in course
-    # Can see assignment
+  test "creating a new project and assigning it" do
+    login @instructor
+    visit new_project_path
+    select("Front End Engineering", from:'Topic')
+    fill_in('Title', with:"Make Coffee")
+    fill_in("Description", with:"First, get grounds. Next, pour over.") 
+    click_button "Create Project"
+    assert page.has_content?("Project created.")
+    @project = Project.last
+    assert page.has_content?('First, get grounds')
+    select(@course.organization, from:'Course')
+    click_button("Create Assignment")
+    assert page.has_content?("")
+    @assignment = Assignment.last
+    login @student
+    assert page.has_content?("First, get grounds.")
   end
 end
